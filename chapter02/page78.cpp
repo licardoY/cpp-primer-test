b5ac1dbe11dbc204aea03e1429c94f61e127d53b
li <licardoooo@gmail.com>
2018-07-03 20:40:22 +0800

    ignore .output ,page78

---
+++ chapter02/page78.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+// 2018-07-03 20:38:23
+int main(int argc, char const *argv[])
+{
+    string word;
+    
+    while(cin >> word){
+        cout << word << endl;
+    }
+    
+    return 0;
+}
